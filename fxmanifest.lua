a6fdb62c5430f52537f5cfb5c639e56fe50cf8b2
ralphijs <95063929+yxupy@users.noreply.github.com>
2022-06-30 19:15:46 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'yxupy loading screen'
+version '1.0'
+author 'yxupy#1337'
+
+lua54 'yes'
+client_script 'client/client.lua'
+
+files { 'ui/*',}
+
+loadscreen { 'ui/index.html' }
+loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes'
